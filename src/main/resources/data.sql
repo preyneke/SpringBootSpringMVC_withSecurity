089e40bb0cc2cd7088917018741289f6cf619f0e
Pieter Reyneke <preyneke@gmail.com>
2018-07-19 15:02:02 +0200

    Finished basic model and view of store

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO `role` VALUES (1,'ADMIN');
 
 INSERT INTO products values ('1', 'SmartPhone', 'New Iphone By Apple', 'Apple','Apple iPhone', '200.55', '100');
-INSERT INTO products values ('2', 'Laptop', 'New phone By Samsung', 'Samsung','Samsung Galexy S9', '400.99', '50');
+INSERT INTO products values ('2', 'Laptop', 'The new laptop from Lenovo, even better than the old one!', 'Lenovo','Lenovo ThinkPad for men', '400.99', '50');
 INSERT INTO products values ('3', 'Audio', 'Wireless headphones designed by Dr Dre', 'Apple','Beats By Dre', '6000.55', '2');
